b66998b87f30e1b8eecedb0581fb82132d4c5cd5
Alex@202-2 <Alex@202-2>
2019-08-28 12:14:41 +0300

    insert done.

--- DataStructures/ForwardList/main.cpp
+++ DataStructures/ForwardList/main.cpp
@@ -7,27 +7,34 @@ class Element
 {
 	int Data;		//Значение элемента
 	Element* pNext;	//Адрес следующего элемента
+	static int count;
 public:
 	Element(int Data, Element* pNext = nullptr)
 	{
 		this->Data = Data;
 		this->pNext = pNext;
+		count++;
 		cout << "EConstructor:\t" << this << endl;
 	}
 	~Element()
 	{
+		count--;
 		cout << "EDestructor:\t" << this << endl;
 	}
 	friend class ForwardList;
 };
 
+int Element::count = 0;
+
 class ForwardList
 {
 	Element* Head;	//Указатель на начало списка
+	unsigned int size;
 public:
 	ForwardList()
 	{
 		this->Head = nullptr;
+		this->size = 0;
 		cout << "LConstructor:\t" << this << endl;
 	}
 	~ForwardList()
@@ -43,10 +50,63 @@ public:
 		Element* New = new Element(Data);
 		New->pNext = Head;
 		Head = New;
+		size++;
 	}
 	void push_back(int Data)
 	{
+		if (Head == nullptr)
+		{
+			push_front(Data);
+			return;
+		}
+		Element* Temp = Head;
+		while (Temp->pNext != nullptr)Temp = Temp->pNext;
+		Temp->pNext = new Element(Data);
+		size++;
+	}
 
+	void insert(int index, int data)
+	{
+		if (index == 0)
+		{
+			push_front(data);
+			return;
+		}
+		if (index > size)
+		{
+			cout << "Error: Out of range." << endl;
+			return;
+		}
+		Element* Temp = Head;
+		for (int i = 0; i < index - 1; i++)
+		{
+			Temp = Temp->pNext;
+		}
+		Element* New = new Element(data);
+		New->pNext = Temp->pNext;
+		Temp->pNext = New;
+		size++;
+	}
+
+	//Удаление элементов:
+	void pop_front()
+	{
+		Element* buffer = Head;
+		Head = Head->pNext;
+		delete buffer;
+		size--;
+	}
+
+	void pop_back()
+	{
+		Element* Temp = Head;
+		while (Temp->pNext->pNext != nullptr)
+		{
+			Temp = Temp->pNext;
+		}
+		delete Temp->pNext;
+		Temp->pNext = nullptr;
+		size--;
 	}
 
 	void print()
@@ -59,6 +119,7 @@ public:
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext;	//Переход на следующий элемент.
 		}
+		cout << "Количество элементво списка: " << size << endl;
 	}
 };
 
@@ -73,5 +134,19 @@ void main()
 	{
 		fl.push_front(rand() % 100);
 	}
+	fl.push_back(123);	fl.print();
+	fl.pop_back();	fl.print();
+
+	int index;
+	int data;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> data;
+	fl.insert(index, data);
 	fl.print();
+
+	ForwardList fl2;
+	fl2.push_back(3);
+	fl2.push_back(5);
+	fl2.push_back(8);
+	fl2.print();
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,6 +1,9 @@
-В ForwardList добавить методы 
-push_back();
-pop_front();
-pop_back();
+В ForwardList добавить метод
+erase() - удаляет значение по индексу.
+
+push_back();	DONE
+pop_front();	DONE
+pop_back(); 	DONE
+insert();	DONE
 
 https://github.com/okovtun/ST_PU_34a
